89b42d68f30f4c18063a7475bfd7ec4e43e24964
Luke Stephens <imahowiie@gmail.com>
2022-02-14 22:21:43 +0200

    vercel

--- pages/api/CurrentlyPlaying.js
+++ pages/api/CurrentlyPlaying.js
@@ -3,9 +3,9 @@ import querystring from 'querystring';
 import process from 'process';
 
 const {
-  NEXT_PUBLIC_SPOTIFYSPOTIFY_CLIENT_ID: client_id,
-  NEXT_PUBLIC_SPOTIFYSPOTIFY_CLIENT_SECRET: client_secret,
-  NEXT_PUBLIC_SPOTIFYSPOTIFY_REFRESH_TOKEN: refresh_token,
+  NEXT_PUBLIC_SPOTIFY_CLIENT_ID: client_id,
+  NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET: client_secret,
+  NEXT_PUBLIC_SPOTIFY_REFRESH_TOKEN: refresh_token,
 } = process.env;
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
